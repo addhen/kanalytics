e2c67a1bcca7f67fa1dfe4032e85245cb6d534d8
Henry Addo <henry@addhen.org>
2024-10-25 11:52:10 -0700

    Minor code clean ups

--- kanalytics-viewer/src/commonMain/kotlin/com/addhen/kanalytics/viewer/app/shared/ui/ViewerAppTheme.kt
+++ kanalytics-viewer/src/commonMain/kotlin/com/addhen/kanalytics/viewer/app/shared/ui/ViewerAppTheme.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -32,6 +34,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.addhen.kanalytics.viewer.app.shared.ui.theme.AppTheme
 import com.addhen.kanalytics.viewer.app.shared.ui.theme.ColorContrast
@@ -132,8 +137,9 @@ private fun EventCard(event: AnalyticsEvent, isExpanded: Boolean, onToggleExpand
         verticalAlignment = Alignment.CenterVertically
       ) {
         Column {
-          Text(event.name, style = MaterialTheme.typography.titleLarge)
+          Text(event.name, style = MaterialTheme.typography.titleMedium)
           Text(event.timestamp, style = MaterialTheme.typography.labelMedium)
+          Spacer(modifier = Modifier.height(8.dp))
           SuggestionChip(
             onClick = { },
             label = { Text(event.provider) },
@@ -151,12 +157,38 @@ private fun EventCard(event: AnalyticsEvent, isExpanded: Boolean, onToggleExpand
         Spacer(modifier = Modifier.height(8.dp))
         HorizontalDivider()
         Spacer(modifier = Modifier.height(8.dp))
-        Spacer(modifier = Modifier.height(4.dp))
-        Text("Key-Value Map:", style = MaterialTheme.typography.titleMedium)
         event.keyValueMap.forEach { (key, value) ->
-          Text("$key: $value", style = MaterialTheme.typography.bodyMedium)
+          KeyValueView(
+            key = key,
+            value = value,
+            textStyle = MaterialTheme.typography.bodyMedium.copy(fontFamily = FontFamily.Monospace),
+            modifier = Modifier.fillMaxWidth()
+          )
         }
       }
     }
+  }
+}
+
+
+@Composable
+private fun KeyValueView(
+  key: String,
+  value: String?,
+  textStyle: TextStyle = MaterialTheme.typography.bodyMedium,
+  modifier: Modifier = Modifier,
+) {
+  Row(modifier = modifier) {
+    Text(
+      text = "$key:",
+      style = textStyle.copy(fontWeight = FontWeight.Bold),
+      modifier = Modifier.widthIn(min = 60.dp).weight(3f)
+    )
+    Spacer(Modifier.width(2.dp))
+    Text(
+      text = value ?: "",
+      style = textStyle,
+      modifier = Modifier.weight(2f)
+    )
   }
 }
