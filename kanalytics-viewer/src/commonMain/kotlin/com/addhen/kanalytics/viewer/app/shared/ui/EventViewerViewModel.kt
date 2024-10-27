55627c16dcee71563285c24941e4ae690623814d
Henry Addo <henry@addhen.org>
2024-10-27 16:33:43 -0700

    Make use of the event repository in the view model

--- kanalytics-viewer/src/commonMain/kotlin/com/addhen/kanalytics/viewer/app/shared/ui/EventViewerViewModel.kt
+++ kanalytics-viewer/src/commonMain/kotlin/com/addhen/kanalytics/viewer/app/shared/ui/EventViewerViewModel.kt
@@ -6,6 +6,8 @@ package com.addhen.kanalytics.viewer.app.shared.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.touchlab.kermit.Logger
+import com.addhen.kanalytics.viewer.app.shared.data.model.EventData
+import com.addhen.kanalytics.viewer.app.shared.data.repository.EventDataRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -16,11 +18,13 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
-public class EventViewerViewModel() : ViewModel() {
+internal class EventViewerViewModel(
+  eventRepository: EventDataRepository,
+) : ViewModel() {
   private val viewStateEmitter =
     MutableStateFlow(LocationUiState(flag = LocationUiState.Flag.LOADING))
 
-  public val viewState: StateFlow<LocationUiState> = viewStateEmitter
+  val viewState: StateFlow<LocationUiState> = viewStateEmitter
     .stateIn(
       viewModelScope,
       started = SharingStarted.WhileSubscribed(5_000),
@@ -28,21 +32,21 @@ public class EventViewerViewModel() : ViewModel() {
     )
 
   init {
-    flow { emit("Hey my location") }
+    eventRepository.getAll(10, 0)
       .distinctUntilChanged()
-      .onEach { state ->
-        Logger.d(EventViewerViewModel::class.simpleName.toString()) { "state $state" }
+      .onEach { eventDataList ->
+        Logger.d(EventViewerViewModel::class.simpleName.toString()) { "state $eventDataList" }
         viewStateEmitter.update { currentUiState ->
           currentUiState.copy(
             flag = LocationUiState.Flag.IDLE,
-            state = state,
+            events = eventDataList,
           )
         }
       }.launchIn(viewModelScope)
   }
 
   public data class LocationUiState(
-      public val state: String = "",
+      public val events: List<EventData> = emptyList(),
       public val flag: Flag = Flag.IDLE,
   ) {
     public enum class Flag {
