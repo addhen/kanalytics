d0c4672c44f8fad3bd4d7a442fe0fb8af8943cf7
Henry Addo <henry@addhen.org>
2025-01-15 15:50:20 -0800

    Configure notifications and implement notification action
    Refactor iOS notification implementation to open viewer app
    
    - Set the notification sound to the default.
    - Set the notification categories.
    - Add a notification delegate to handle the response.
    - Launch the viewer app when the notification action is triggered.
    - Removes all delivered notifications when `clearBuffer` is called.

--- kanalytics-viewer/src/iosMain/kotlin/com/addhen/kanalytics/viewer/app/NotificationManager.ios.kt
+++ kanalytics-viewer/src/iosMain/kotlin/com/addhen/kanalytics/viewer/app/NotificationManager.ios.kt
@@ -4,23 +4,44 @@
 package com.addhen.kanalytics.viewer.app
 
 import co.touchlab.kermit.Logger
+import com.addhen.kanalytics.viewer.app.shared.ui.launchViewerApp
 import platform.Foundation.NSError
 import platform.UserNotifications.UNAuthorizationOptionAlert
 import platform.UserNotifications.UNAuthorizationOptionBadge
 import platform.UserNotifications.UNAuthorizationOptionSound
 import platform.UserNotifications.UNMutableNotificationContent
+import platform.UserNotifications.UNNotificationAction
+import platform.UserNotifications.UNNotificationActionOptionForeground
+import platform.UserNotifications.UNNotificationCategory
+import platform.UserNotifications.UNNotificationCategoryOptionNone
 import platform.UserNotifications.UNNotificationRequest
+import platform.UserNotifications.UNNotificationResponse
+import platform.UserNotifications.UNNotificationSound
 import platform.UserNotifications.UNUserNotificationCenter
+import platform.UserNotifications.UNUserNotificationCenterDelegateProtocol
+import platform.darwin.NSObject
 
 internal actual fun NotificationManager(): NotificationManager {
   return NotificationManagerImpl()
 }
 
 internal class NotificationManagerImpl : NotificationManager {
-  private val notificationId: String = "com.gyanoba.inspektor.notification"
+  private val notificationId: String = "com.addhen.kanalytics.notification"
+
+  private val notificationAction = UNNotificationAction.actionWithIdentifier(
+    identifier = "OPEN_VIEWER_APP_ACTION",
+    title = "Open Viewer App",
+    options = UNNotificationActionOptionForeground
+  )
+
+  private val notificationCategory = UNNotificationCategory.categoryWithIdentifier(
+    identifier = "KANALYTICS_CATEGORY",
+    actions = listOf(notificationAction),
+    intentIdentifiers = listOf<String>(),
+    options = UNNotificationCategoryOptionNone
+  )
 
   override fun showNotification(eventName: String, trackerName: String) {
-    // Implement notification using notification center
     val notificationCenter = UNUserNotificationCenter.currentNotificationCenter()
     try {
       // Requesting notification permissions
@@ -39,6 +60,8 @@ internal class NotificationManagerImpl : NotificationManager {
       val content = UNMutableNotificationContent().apply {
         setTitle(eventName)
         setBody(trackerName)
+        setCategoryIdentifier("KANALYTICS_CATEGORY")
+        setSound(UNNotificationSound.defaultSound())
       }
 
       // Create a new notification
@@ -48,11 +71,27 @@ internal class NotificationManagerImpl : NotificationManager {
         null,
       )
 
+      notificationCenter.setNotificationCategories(setOf(notificationCategory))
       notificationCenter.addNotificationRequest(request) { error ->
         if (error != null) {
           Logger.e(error.toThrowable()) {
             "Error adding notification request: ${error.localizedDescription}"
           }
+        } else {
+          Logger.i { "Notification request added successfully." }
+        }
+      }
+
+      notificationCenter.delegate = object : NSObject(), UNUserNotificationCenterDelegateProtocol {
+        override fun userNotificationCenter(
+          center: UNUserNotificationCenter,
+          didReceiveNotificationResponse: UNNotificationResponse,
+          withCompletionHandler: () -> Unit
+        ) {
+          if (didReceiveNotificationResponse.actionIdentifier == "OPEN_VIEWER_APP_ACTION") {
+            launchViewerApp()
+          }
+          withCompletionHandler()
         }
       }
     } catch (e: IllegalStateException) {
@@ -62,8 +101,8 @@ internal class NotificationManagerImpl : NotificationManager {
 
   override fun clearBuffer() {
     // Clear the notification buffer
-    //val notificationCenter = UNUserNotificationCenter.currentNotificationCenter()
-    //notificationCenter.removeDeliveredNotifications(listOf(notificationId))
+    val notificationCenter = UNUserNotificationCenter.currentNotificationCenter()
+    notificationCenter.removeDeliveredNotificationsWithIdentifiers(listOf(notificationId))
   }
 
   private fun NSError.toThrowable(): Throwable {
